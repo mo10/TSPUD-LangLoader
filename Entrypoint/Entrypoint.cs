a37346b6677ffcf0340ed169be225a140acb3d68
某10 <0w0@0w0.pw>
2022-05-30 01:23:16 +0800

    Add Entrypoint project

---
+++ Entrypoint/Entrypoint.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace Entrypoint
+{
+    public class Entrypoint
+    {
+        public static void Start()
+        {
+            File.WriteAllText("doorstop_hello.log", "Hello from Unity!");
+        }
+    }
+}
