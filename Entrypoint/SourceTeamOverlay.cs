6e162ba86d19c1cc2284b1eff366d7b77491fa8f
Rui Geng <0w0geng@gmail.com>
2023-04-29 00:40:09 +0800

    Remove watermark. Optimize zoom.

--- Entrypoint/SourceTeamOverlay.cs
+++ Entrypoint/SourceTeamOverlay.cs
@@ -1,5 +1,4 @@
-﻿using Steamworks;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -12,65 +11,48 @@ namespace Entrypoint
 {
     class SourceTeamOverlay : MonoBehaviour
     {
-        public Text textTL;
-        //public Text textTC;
-        public Text textTR;
-        public Text textBL;
-        //public Text textBC;
-        public Text textBR;
-        private bool isSteamInited = false;
-        float zoom_step = 5f;
+
+        float zoom_step = 6f;
         float zoom_stop = 0f;
         bool zoom_down = false;
+        float default_mouse_sensX;
+        float default_mouse_sensY;
 
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoInlining)]
         void Awake()
         {
             var canvas = gameObject.AddComponent<Canvas>();
             var canvasScaler = gameObject.AddComponent<CanvasScaler>();
-            //var graphicRaycaster = gameObject.AddComponent<GraphicRaycaster>();
-
             // Canvas
             canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-
-            textTL = CreateText(new Vector2(0, 1), new Vector2(0, 1), new Vector2(0, 1), TextAnchor.UpperLeft);
-            //textTC = CreateText(new Vector2(0.5f, 1), new Vector2(0.5f, 1), new Vector2(0.5f, 1), TextAnchor.UpperCenter);
-            textTR = CreateText(new Vector2(1, 1), new Vector2(1, 1), new Vector2(1, 1), TextAnchor.UpperRight);
-
-            textBL = CreateText(new Vector2(0, 0), new Vector2(0, 0), new Vector2(0, 0), TextAnchor.LowerLeft);
-            //textBC = CreateText(new Vector2(0.5f, 0), new Vector2(0.5f, 0), new Vector2(0.5f, 0), TextAnchor.LowerCenter);
-            textBR = CreateText(new Vector2(1, 0), new Vector2(1, 0), new Vector2(1, 0), TextAnchor.LowerRight);
-
-            Logger.Debug("Awake");
         }
         [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoInlining)]
         void Update()
         {
-            if (SteamManager.Initialized && !isSteamInited)
-            {
-                isSteamInited = true;
-                textBL.text = SteamUser.GetSteamID().ToString();
-                Logger.Debug("Update");
-            }
             if (StanleyController.Instance == null)
                 return;
 
+            // 按Z缩放
             if (Input.GetKeyDown(KeyCode.Z))
             {
                 zoom_down = true;
-                zoom_stop = StanleyController.Instance.FieldOfViewBase / 2;
-                //StanleyController.Instance.FieldOfView = 10;
-
-                //MainCamera.Camera.fieldOfView = (MainCamera.Camera.fieldOfView / 2);
-                //Logger.Debug("Zoom in");
+                zoom_stop = StanleyController.Instance.FieldOfViewBase / 2f;
+                default_mouse_sensX = StanleyController.Instance.mouseSensitivityX;
+                default_mouse_sensY = StanleyController.Instance.mouseSensitivityY;
+                StanleyController.Instance.mouseSensitivityX = default_mouse_sensX * 0.5f;
+                StanleyController.Instance.mouseSensitivityY = default_mouse_sensY * 0.5f;
+                StanleyController.Instance.SetMovementSpeedMultiplier(0.5f);
             }
             if (Input.GetKeyUp(KeyCode.Z))
             {
                 zoom_down = false;
                 zoom_stop = StanleyController.Instance.FieldOfViewBase;
-                //Logger.Debug("Zoom out");
+                StanleyController.Instance.mouseSensitivityX = default_mouse_sensX;
+                StanleyController.Instance.mouseSensitivityY = default_mouse_sensY;
+                StanleyController.Instance.SetMovementSpeedMultiplier(1.0f);
             }
-            if(zoom_down && StanleyController.Instance.FieldOfView >= zoom_stop)
+
+            if (zoom_down && StanleyController.Instance.FieldOfView >= zoom_stop)
             {
                 StanleyController.Instance.FieldOfView -= zoom_step;
             }
@@ -78,12 +60,7 @@ namespace Entrypoint
             {
                 StanleyController.Instance.FieldOfView += zoom_step;
             }
-            //else if (zoom_stop !=0 && StanleyController.Instance.FieldOfView != zoom_stop)
-            //{
-            //    StanleyController.Instance.FieldOfView = zoom_stop;
-            //    zoom_stop = 0;
-            //}
-
+#if DEBUG
             if (Input.GetKeyDown(KeyCode.LeftShift))
             {
                 StanleyController.Instance.SetMovementSpeedMultiplier(3.0f);
@@ -92,29 +69,7 @@ namespace Entrypoint
             {
                 StanleyController.Instance.SetMovementSpeedMultiplier(1.0f);
             }
-        }
-
-        Text CreateText(Vector2 pivot, Vector2 anchorMin, Vector2 anchorMax, TextAnchor textAnchor)
-        {
-            var textGameObject = new GameObject("Watermark");
-            DontDestroyOnLoad(textGameObject);
-            textGameObject.transform.parent = gameObject.transform;
-
-            var text = textGameObject.AddComponent<Text>();
-            text.font = AssetManager.Get<Font>("SourceHanSans");
-            text.color = new Color(1, 1, 1, 0.2f);
-            text.text = $"起源汉化组 测试版本";
-            text.fontSize = 30;
-            text.alignment = textAnchor;
-
-            var rect = textGameObject.GetComponent<RectTransform>();
-            rect.localPosition = new Vector3(0, 0, 0);
-            rect.sizeDelta = new Vector2(350, 150);
-            rect.pivot = pivot;
-            rect.anchorMin = anchorMin;
-            rect.anchorMax = anchorMax;
-
-            return text;
+#endif
         }
     }
 }
